350dd338c66fdfe240450a0a5dcb77428c57742d
SAI KUMAR@LAPTOP-52G4EK29 <saikumarkaicherla2004@gmail.com>
2024-07-01 17:55:05 +0530

    updated

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -7,7 +7,7 @@ function Hero(){
         <div className="hero-text">
             <h1>We Ensure better education for a better world</h1>
             <p>Our cutting-edge curriculum is designed to empower students with the knowledge, skills, and experiences needed to excel in the dynamic field of education</p>
-             <button className="btn">Explore More <img src={dark_arrow}  /></button>
+             <button className="btn">Explore More <img src={dark_arrow} alt='' /></button>
         </div>
 
       </div>
